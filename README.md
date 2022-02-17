93d145385d243929cc4b1c79a7acf6102a597f2b
David Lyhed Danielsson <davidlyheddanielsson@gmail.com>
2022-02-17 12:38:58 +0100

    feat: some sort of web/local build hybrid setup

---
+++ README.md
@@ -0,0 +1,2 @@
+Compilation:
+    - Update .vscode/settings.json environment variable to reflect your environment
--- main.cpp
+++ main.cpp
@@ -1,7 +1,60 @@
-#include "raylib.h"
-#include <filesystem>
+#include <raylib.h>
+#include <chrono>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <array>
 
-const static std::filesystem::path ASSET_ROOT(DASSET_ROOT);
+#ifdef PLATFORM_WEB
+#include <emscripten/emscripten.h>
+#endif
+
+// Windows clocks in at 260, linux allows 4096
+constexpr int MAX_PATH_LENGTH = 256;
+std::array<char, MAX_PATH_LENGTH> pathBuffer = {0}; // Helper to avoid dynamic allocation
+std::array<char, MAX_PATH_LENGTH> AssetPath(const char *assetName)
+{
+    snprintf(pathBuffer.data(), MAX_PATH_LENGTH, "%s/%s", DASSET_ROOT, assetName);
+    return pathBuffer;
+}
+
+Camera camera = {0};
+Model model;
+
+std::chrono::time_point<std::chrono::high_resolution_clock> last;
+
+constexpr float TICK_RATE = 1000.0f / 60.0f;
+float timer = 0.0f;
+
+void main_loop()
+{
+    auto now = std::chrono::high_resolution_clock::now();
+    float deltaMs = std::chrono::duration<float, std::milli>(now - last).count();
+    last = now;
+    timer += deltaMs;
+
+    float rotation = (3.141593f * 2.0f) * (timer / 5000.0f);
+
+    camera.position = (Vector3){cosf(rotation) * 10.0f, 5.0f, sinf(rotation) * 10.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    char buf[128];
+    sprintf(buf, "Frame time: %f", deltaMs);
+    DrawText(buf, 0, 0, 20, LIGHTGRAY);
+
+    BeginMode3D(camera);
+    DrawModel(model, (Vector3){0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
+    EndMode3D();
+
+    EndDrawing();
+}
 
 int main()
 {
@@ -10,29 +63,18 @@ int main()
 
     InitWindow(screenWidth, screenHeight, "Raylib test");
 
+    model = LoadModel(AssetPath("Bob/glTF/Bob.gltf").data());
+
+#ifdef PLATFORM_WEB
+    emscripten_set_main_loop(main_loop, 0, 1);
+#else
     SetTargetFPS(60);
-
-    Camera camera = {0};
-    camera.position = (Vector3){5.0f, 5.0f, 5.0f};
-    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
-    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
-    camera.fovy = 45.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-
-    Model model = LoadModel((ASSET_ROOT / "Bob/glTF/Bob.gltf").c_str());
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-        DrawModel(model, (Vector3){0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
-        EndMode3D();
-
-        EndDrawing();
+        main_loop();
     }
+#endif
 
     CloseWindow();
 
