02cd60d271ce3b8d593fa85f9a21d92891626a21
David Lyhed Danielsson <davidlyheddanielsson@gmail.com>
2022-04-19 17:59:56 +0200

    fix: typo

--- src/imgui/imgui_internal.hpp
+++ src/imgui/imgui_internal.hpp
@@ -3,7 +3,7 @@
 #include <imgui.h>
 
 // Including imgui's imgui_internal.h creates a bunch of function overloads and
-// such - which breaks things - so a this local imgui_internal.hpp is used to
+// such - which breaks things - so this local imgui_internal.hpp is used to
 // hide the implementation details
 
 bool ValidStackSize(ImGuiContext*);
