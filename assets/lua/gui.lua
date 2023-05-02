b70792ce87c3d00c2892863d4018c3a2c7b3ee4e
David Lyhed Danielsson <davidlyheddanielsson@gmail.com>
2023-05-02 16:45:14 +0200

    refactor: move RecursivePrint to top of file

--- assets/lua/gui.lua
+++ assets/lua/gui.lua
@@ -5,6 +5,28 @@ function AddComponentOrPrintError(...)
     end
 end
 
+function RecursivePrint(tab, indent)
+    if tab == nil then
+        print(string.rep("  ", indent), "NIL")
+        return
+    end
+    if type(tab) ~= "table" then
+        print(string.rep("  ", indent), tab)
+    else
+        for key, value in pairs(tab) do
+            if type(value) == "table" then
+                print(string.rep("  ", indent), key, " = {")
+                RecursivePrint(value, indent + 1)
+                print(string.rep("  ", indent), "},")
+            elseif type(value) == "string" then
+                print(string.rep("  ", indent), key, " = \"", value, "\",")
+            else
+                print(string.rep("  ", indent), key, " = ", value, ",")
+            end
+        end
+    end
+end
+
 function SaveLevel()
     local file = io.open("../assets/default_level.lua", "w+")
     file:write("return{")
@@ -331,28 +353,6 @@ function imgui()
         usingGizmo = IsUsingGizmo()
     else
         usingGizmo = false
-    end
-
-    function RecursivePrint(tab, indent)
-        if tab == nil then
-            print(string.rep("  ", indent), "NIL")
-            return
-        end
-        if type(tab) ~= "table" then
-            print(string.rep("  ", indent), tab)
-        else
-            for key, value in pairs(tab) do
-                if type(value) == "table" then
-                    print(string.rep("  ", indent), key, " = {")
-                    RecursivePrint(value, indent + 1)
-                    print(string.rep("  ", indent), "},")
-                elseif type(value) == "string" then
-                    print(string.rep("  ", indent), key, " = \"", value, "\",")
-                else
-                    print(string.rep("  ", indent), key, " = ", value, ",")
-                end
-            end
-        end
     end
 
     function RecursiveWrite(file, tab)
