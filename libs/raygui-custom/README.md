c55e0d856fb971ceb740b31d8b3b751ee8d15f35
David Lyhed Danielsson <davidlyheddanielsson@gmail.com>
2023-05-31 16:55:38 +0200

    chore: "fork" raygui
    See readme for details

---
+++ libs/raygui-custom/README.md
@@ -0,0 +1,1 @@
+I made changes that I don't feel like making PRs for, so this is my "branch"/"fork" of raygui
---
+++ src/external/raygui.cpp
@@ -0,0 +1,7 @@
+#include "./raylib.hpp"
+
+#define RAYGUI_IMPLEMENTATION
+
+extern "C" {
+#include "../../libs/raygui-custom/raygui.h"
+}
