a300ae363d17eb29e9225f6886535a2ca3aa55ab
David Lyhed Danielsson <davidlyheddanielsson@gmail.com>
2023-06-04 09:39:13 +0200

    feat: new level + fixes to previous levels
    Fixes due to previous commit(s)

---
+++ assets/levels/test.lua
@@ -0,0 +1,9 @@
+return {
+    [1] = { Tile = {}, Walkable = {}, Render = { assetName = "Floor_Standard", }, Transform = {
+        position = { x = -1, y = 0, z = -1, }, rotation = { x = 0, y = 0, z = 0, }, }, },
+    [2] = { Render = { assetName = "Floor_Standard", }, Transform = { position = { x = -3, y = 0, z = -1, },
+        rotation = { x = 0, y = -0, z = 0, }, }, Tile = {}, Walkable = {}, EnemyGoal = { ids = { 0, 1, 2, }, }, },
+    [1048579] = { EnemySpawn = { goalId = 0, id = 0, }, Render = { assetName = "Floor_Standard", }, Transform = {
+        position = { x = 1, y = 0, z = -1, }, rotation = { x = 0, y = 0, z = 0, }, }, Tile = {}, Walkable = {}, },
+    [0] = { Camera = { up = { x = 0, y = 1, z = 0, }, projection = 0, target = { x = 0, y = 0, z = 0, }, fovy = 45, }, Transform = {
+        position = { x = 10, y = 10, z = 10, }, rotation = { x = 0, y = 0, z = 0, }, }, }, }
